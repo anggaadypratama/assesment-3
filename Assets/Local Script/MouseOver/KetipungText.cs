2c4d87e24b4b92d20a7b0a14263e85a6075cd6c2
Angga <anggaadypratama@outlook.com>
2020-12-21 16:24:39 +0700

    adjust information button

---
+++ Assets/Local Script/MouseOver/KetipungText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KetipungText : MonoBehaviour
+{
+
+    public GameObject ketipungText;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        ketipungText.SetActive(false);
+    }
+
+    public void OnMouseOver(){
+        ketipungText.SetActive(true);
+    }
+
+    public void OnMouseExit(){
+        ketipungText.SetActive(false);
+    }
+
+}
