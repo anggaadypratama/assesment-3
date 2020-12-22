baff880d830455ddf541a3b2fd6a9a62c62a0464
Angga <anggaadypratama@outlook.com>
2020-12-22 11:44:49 +0700

    add menu & and song

---
+++ Assets/Local Script/MouseOver/KetipungSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KetipungSound : MonoBehaviour
+{
+
+    public GameObject ketipungSound;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        ketipungSound.SetActive(false);
+    }
+
+    public void OnMouseOver(){
+        ketipungSound.SetActive(true);
+    }
+
+    public void OnMouseExit(){
+        ketipungSound.SetActive(false);
+    }
+
+}
---
+++ Assets/Local Script/MouseOver/TifaSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TifaSound : MonoBehaviour
+{
+
+    public GameObject tifaSound;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        tifaSound.SetActive(false);
+    }
+
+    public void OnMouseOver(){
+        tifaSound.SetActive(true);
+    }
+
+    public void OnMouseExit(){
+        tifaSound.SetActive(false);
+    }
+
+}
